5ecf89844a259a6e374a8a78ba00c04c5637fb2f
Eugene Berezin <35341885+Eugeneberezin@users.noreply.github.com>
2021-12-07 18:28:46 -0700

    in progress

--- SwiftUIcarousel/SwiftUIcarousel/Service/Service.swift
+++ SwiftUIcarousel/SwiftUIcarousel/Service/Service.swift
@@ -4,12 +4,12 @@ class Service {
     
     static let shared = Service() // singleton
     
-    func fetchMovies(searchTerm: String, completion: @escaping ([Result], Error?) -> ()) {
+    func fetchMovies(searchTerm: String, page: Int = 1, completion: @escaping ([Result], Error?) -> ()) {
         print("Fetching movies!")
         
         let API_KEY = "876db37c9eb2be92a162285d2d985373"
         
-        let urlString = "https://api.themoviedb.org/3/search/movie?query=\(searchTerm)&page=1&api_key=\(API_KEY)"
+        let urlString = "https://api.themoviedb.org/3/search/movie?query=\(searchTerm)&page=\(String(page))&api_key=\(API_KEY)"
         guard let url = URL(string: urlString) else { return }
         
         // fetch data from internet
--- SwiftUIcarousel/SwiftUIcarousel/ViweModel/ViewModel.swift
+++ SwiftUIcarousel/SwiftUIcarousel/ViweModel/ViewModel.swift
@@ -7,11 +7,60 @@
 
 import SwiftUI
 
-class ViewModel: ObservableObject {
+enum LoadStatus {
+    case ready (nextPage: Int)
+    case loading (page: Int)
+    case error
+    case done
+}
+
+class ViewModel: ObservableObject, RandomAccessCollection {
     @Published var movies: [Result] = []
     @Published var isTabBarDisplayed = true
     
+    typealias Element = Result
+    
+    subscript(position: Int) -> Result {
+        return movies[position]
+    }
+    
+    var startIndex: Int {movies.startIndex}
+    var endIndex: Int {movies.endIndex}
+    var loadingStatus = LoadStatus.ready(nextPage: 1)
+    
     init() {}
+    
+    func shouldLoadMoreData(currentItem: Result? = nil) -> Bool {
+        guard let currentItem = currentItem else {
+            return true
+        }
+        
+        for n in (movies.count - 3)...(movies.count - 1) {
+            if currentItem.id == movies[n].id {
+                return true
+            }
+        }
+        return false
+    }
+    
+    func loadMoreMovies(searchTern: String, currentItem: Result? = nil) {
+        if !shouldLoadMoreData(currentItem: currentItem) {
+            return
+        }
+        
+        guard case let .ready(page) = loadingStatus else {
+            return
+        }
+        
+        loadingStatus = .loading(page: page)
+        
+        print(page)
+        
+        Service.shared.fetchMovies(searchTerm: searchTern, page: page) { results, error in
+            
+        }
+        
+    }
     
     func getMovies(searchTerm: String) {
         Service.shared.fetchMovies(searchTerm: searchTerm) { movies, _ in
